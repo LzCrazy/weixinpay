03a81309db1786c1013ec07d9d6746ab7bfa8838
LzCrazy <mizur@sina.com>
2017-05-19 22:17:46 +0800

    app

---
+++ app/src/main/java/com/alpha/live/Constants.java
@@ -0,0 +1,16 @@
+package com.alpha.live;
+
+public class Constants {
+
+
+    //appid 微信分配的公众账号ID
+    public static final String APP_ID = "wx14af47ab474a0991";
+
+    //商户号 微信分配的商户号ID
+    public static final String MCH_ID = "1415675302";
+
+    //  API密钥，在商户平台设置
+    public static final  String API_KEY= "ganzhoualpha112114115WXZHOUALPHA";
+
+
+}
